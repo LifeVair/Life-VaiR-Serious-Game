58363e699934311ca709a0d7865abbd182cf46e9
Alexandre DESBOS <alexandre@macbook-pro-de-franck.local>
2023-02-17 16:15:23 +0100

    Adding WIP SQLite DB Manager

---
+++ Source/LifeVair/DatabaseManager.cpp
@@ -0,0 +1,37 @@
+// Copyright : OK
+#include "DatabaseManager.h"
+
+TArray<FString>& UDatabaseManager::getData(const FString Path)
+{
+	
+	FSQLiteDatabase* SQLiteDB = new FSQLiteDatabase();
+	SQLiteDB->Open(*Path, ESQLiteDatabaseOpenMode::ReadOnly);
+	static TArray<FString> ResultColumns;
+	UE_LOG(LogTemp, Warning, TEXT("Error while trying to access the DB"))
+	
+	if(SQLiteDB->IsValid())
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 50.0f, FColor::Green, TEXT("COnnected to DB"));
+		const FString Query = TEXT("select * from Domains");
+		FSQLitePreparedStatement LoadStatement;
+		LoadStatement.Reset();
+		GEngine->AddOnScreenDebugMessage(-1, 50.0f, FColor::Blue, TEXT("Querying"));
+		
+		LoadStatement.Create(*SQLiteDB,*Query, ESQLitePreparedStatementFlags::Persistent);
+
+		//IsSuccessful = LoadStatement.IsValid();
+		
+		ResultColumns = LoadStatement.GetColumnNames();
+
+		LoadStatement.ClearBindings();
+		LoadStatement.Destroy();
+		//TODO. Do some checks to avoid crash
+		SQLiteDB->Close();
+		delete SQLiteDB;
+		
+		return ResultColumns;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Error while trying to access the DB"))
+
+	return ResultColumns;
+}
---
+++ Source/LifeVair/DatabaseManager.h
@@ -0,0 +1,17 @@
+// Copyright : OK
+
+#pragma once
+
+#include "SQLiteDatabase.h"
+#include "CoreMinimal.h"
+#include "DatabaseManager.generated.h"
+
+UCLASS(Blueprintable, DisplayName="SQLite DB Manager")
+class LIFEVAIR_API UDatabaseManager : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, meta=(CompactNodeTitle=""), Category="SQLite DB Manager ")
+	TArray<FString>& getData(const FString Path);
+};
